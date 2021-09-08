e5192ec5864722a3abb2ebc9df77ddecd28018d3
unknown <fizz.zehra14@gmail.com>
2021-09-08 15:21:21 -0500

    creating user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,33 +1,58 @@
-const { User,Thought }=require('../models');
+const { createSecureServer } = require('http2');
+const { User, Thought } = require('../models');
 
-const userController={
+const userController = {
 
-// GET all users
-getAllUser(req,res){
-    User.find({})
-    .then(dbUserData=>res.json(dbUserData))
-    .catch(err=>
-        {
-            console.log(err);
-            res.status(400).json(err)
-        });
-},
+    // GET all users
+    getAllUser(req, res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            });
+    },
 
-// GET a single user by its _id and populated thought and friend data
-getUserById({params},res){
-User.findOne({ _id:params.id})
-.then(dbUserData=>{
-    if(!dbUserData){
+    // GET a single user by its _id and populated thought and friend data
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'Opps! No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            });
+    },
+
+    // POST a new user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            });
+    },
+
+    // PUT to update a user by its _id
+    updateUserById({params,body},res){
+User.findOneAndUpdate({_id:params.id},body,{new:true})//new:true returns the updated version
+.then(dbUserData => {
+    if (!dbUserData) {
         res.status(404).json({ message: 'Opps! No user found with this id!' });
         return;
     }
     res.json(dbUserData);
 })
-.catch(err=>
-    {
-        console.log(err);
-        res.status(400).json(err)
-    });
-}
+.catch(err => {
+    console.log(err);
+    res.status(400).json(err)
+});    }
+
 
 }
