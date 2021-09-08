f9113e78135d925d663d2fc5e38ebc9adff222ac
unknown <fizz.zehra14@gmail.com>
2021-09-07 23:14:51 -0500

    CREATING USER MODEL

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
+//Dependencies
 const { schema, model } = require('mongoose');
 
-const User = new schema(
+//Creates the user schema
+const user_schema = new schema(
     {
         username:
         {
@@ -16,18 +18,33 @@ const User = new schema(
             required: true,
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
         },
-        thoughts:
+        thoughts:[
         {
             type: schema.Types.ObjectId,
             ref: 'Thought'
-        },
+        }],
         friends: [
         {
             type: schema.Types.ObjectId,
             ref: 'User'
         }
         ]
-
-
+    },
+    {
+        toJSON:{
+            virtuals:true
+        },
+        id:false
     }
-)
+);
+
+//virtual that retrieves the length of the user's friends 
+user_schema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
+
+// create the User Model using the User's Schema
+const User=model('User',user_schema);
+
+// export the User model
+module.exports=User;
