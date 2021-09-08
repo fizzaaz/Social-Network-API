8a978c902943893331ee2a467463c4032871d068
unknown <fizz.zehra14@gmail.com>
2021-09-08 14:24:18 -0500

    CREATING reaction MODEL

--- models/reaction.js
+++ models/reaction.js
@@ -7,6 +7,33 @@ const react_schema=new schema(
         {
             id:schema.Types.ObjectId,
             default:()=>new Types.ObjectId
+        },
+        reactionBody:
+        {
+            type:String,
+            required:true,
+            maxlength:280
+        },
+        username:
+        {
+            String:true,
+            required:true
+        },
+        createdAt:
+        {
+            type:Date,
+            default:Date.now,
+            get: createdAtVal=>dateFormat(createdAtVal)
         }
+    },
+    {
+        toJSON:
+        {
+            getters:true
+        },
+        id:false
     }
-)
+);
+
+const Reaction=model('reaction',react_schema)
+module.exports=Reaction;
