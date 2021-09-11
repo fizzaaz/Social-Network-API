684b755a640d26a167bc596d3169daa09fd32f05
unknown <fizz.zehra14@gmail.com>
2021-09-11 15:20:38 -0500

    creating user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,27 @@
+const router=require('express').Router();
+
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUserById,
+    deleteUserById,
+    addFriendtoList,
+    removeFriend
+}=require('../../controllers/user-controller');
+
+router
+.route('/')
+.get(getAllUser)
+.post(createUser);
+
+router.route('/:id')
+.get(getUserById)
+.put(updateUserById)
+.delete(deleteUserById)
+
+router.route(':userId/friends/:friendId')
+.post(addFriendtoList)
+.delete(removeFriend)
+
+module.exports=router;
